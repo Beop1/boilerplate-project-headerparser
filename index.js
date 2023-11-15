b6dbb4c7dce290fdb4bd37f25cb98edf59e63351
Bernardo <bernardo.vpp@gmail.com>
2023-11-15 02:25:26 -0300

    1

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.ip || req.connection.remoteAddress;
+  const language = req.acceptsLanguages() || 'unknown';
+  const software = req.headers['user-agent'] || 'unknown';
+  res.json({ipaddress, language, software});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
